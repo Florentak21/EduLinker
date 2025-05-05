84835ca79d16daa8e93ff2008b08a46063dc4ad2
florentak <florentkossiayidedji@gmail.com>
2025-05-05 15:54:14 +0100

    feat: ajout des fichiers de base pour certains composants

---
+++ app/Core/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Core;
+
+abstract class Controller {
+    /**
+     * mon controller de base
+     * il contient le code de base que tous mes controllers auront.
+     * 
+     * */
+}
---
+++ app/Core/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Core;
+
+abstract class Model {
+    /**
+     * mon model de base
+     * il contient le code de base que tous mes models auront.
+     * 
+     * */
+}
